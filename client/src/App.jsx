09dc28d8d1e3c9caa705c85f8dd4afa93eb06d74
JochenSunaert <Jochen.sunaert@student.ehb.be>
2025-05-28 16:21:15 +0200

    404 fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Client from "./components/Client.jsx";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <Routes>
         
         <Route path="/" element={<Home />} />
