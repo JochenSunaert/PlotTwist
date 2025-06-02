4c607359a958f6abc065f33005216b073232293c
JochenSunaert <Jochen.sunaert@student.ehb.be>
2025-06-02 22:16:50 +0200

    update: Change of timer

--- server/server/game.js
+++ server/server/game.js
@@ -118,7 +118,7 @@ function startRound(io, roomCode, gameStates, rooms, roundNumber) {
 
   gameState.promptTimer = startTimer(
     /*timertijd*/
-    20, // Prompt timer duration
+    70, // Prompt timer duration
     (timeLeft) => {
       console.log(`⏳ Timer: ${timeLeft}s remaining for room ${roomCode}`);
       io.to(roomCode).emit("timer-update", timeLeft);
@@ -389,7 +389,7 @@ async function processAllAnswers(io, roomCode, gameStates, rooms) {
 // It emits updates to the clients and handles the end of the phase.
 function startAnswerPhase(io, roomCode, gameStates, rooms) {
   /*timertijd */
-  const timerDuration = 20;
+  const timerDuration = 96;
   let timeLeft = timerDuration;
 
   const gameState = gameStates[roomCode];
